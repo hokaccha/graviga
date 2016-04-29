d2ba2e7eb3ce3eaac56f37df7ee50fc86f21adb8
Kazuhito Hokamura <k.hokamura@gmail.com>
2016-04-29 16:19:30 +0900

    test: add a spec

--- spec/non_null_spec.rb
+++ spec/non_null_spec.rb
@@ -33,4 +33,15 @@ describe 'non null' do
     expect(error).to be_a Graviga::ExecutionError
     expect(error.message).to eq 'Cannot return null for non-nullable field Query.bar.'
   end
+
+  context 'when foo returns some value' do
+    before do
+      allow_any_instance_of(Sandbox::QueryType).to receive(:foo).and_return('x')
+    end
+
+    it 'should not be error' do
+      result = schema.execute('{ foo }')
+      expect(result).to eq({ data: { foo: 'x' } })
+    end
+  end
 end
